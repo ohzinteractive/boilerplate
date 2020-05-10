d277ffcaf35a49815d87e27369806bc899379863
Ramiro Fages <fages_fages@hotmail.com>
2020-05-10 07:30:51 -0300

    updated datgui controls

--- app/libs/ui_datgui.js
+++ app/libs/ui_datgui.js
@@ -1,16 +1,6 @@
 
 var settings = {
-  "current_orientation": 7,
-  "current_tilt": 10,
-  "wrapping_fov" : 180,
-  "use_upper_hemisphere": false,
-  "new_orientation": 7,
-  "new_tilt": 10,
-  "texture_rotation_x": "0",
-  "texture_rotation_y": "0",
-  "texture_rotation_z": "0",
-  "marker_orientation" : 0,
-  "marker_tilt" : 0
+  "scale" : 0.5
 }
 
 
@@ -19,49 +9,15 @@ var settings = {
 
 
 var gui = new dat.GUI();
+document.querySelector(".dg.ac").style["z-index"] = 999
 
 
 
-var general = {
-
-  "Apply settings": function(){
-
-  },
-
-
-}
+gui.add(settings,"scale", 0.1, 2)
 
 
 
-gui.add(settings,"current_orientation")
-gui.add(settings,"current_tilt")
-gui.add(settings,"wrapping_fov", 10, 360);
-gui.add(settings,"use_upper_hemisphere");
-gui.add(settings,"new_orientation", 0, 360);
-gui.add(settings,"new_tilt", 0, 90);
 
-gui.add(settings,"texture_rotation_x").onChange(function(val){
-
-});
-gui.add(settings,"texture_rotation_y").onChange(function(val){
-
-});
-gui.add(settings,"texture_rotation_z").onChange(function(val){
-
-});
-
-
-gui.add(settings,"marker_orientation")
-gui.add(settings,"marker_tilt")
-
-gui.add(general, 'Apply settings')
-
-gui.close()
-
-
-// gui.add(general, 'Save config')
-// gui.add(general, 'Restore default config')
-// gui.add(general, 'Export conf to JSON')
 
 
 // gui.close();
