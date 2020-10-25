40015150760d05a47e40db4dcc349b20fd485543
Federico Gratti <federicogratti@gmail.com>
2020-10-25 16:30:39 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         'Cesium': 'readonly'
     },
     'parserOptions': {
-        'ecmaVersion': 2021,
+        'ecmaVersion': 2020,
         'sourceType': 'module'
     },
     'rules': {
