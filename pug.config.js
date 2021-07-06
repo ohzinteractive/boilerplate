284d64548558bf6fc1bde5f01aaadce822278c68
Federico Gratti <federicogratti@gmail.com>
2021-07-06 17:26:03 +0200

    Update pug.config.js

--- pug.config.js
+++ pug.config.js
@@ -5,6 +5,7 @@ let load_json = (file_path) =>
 
 module.exports = {
   locals: {
-    example: load_json('./assets/data/example.json')
+    example: load_json('./assets/data/example.json'),
+    package: load_json('./package.json')
   }
 };
