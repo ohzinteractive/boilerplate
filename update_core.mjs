70d887f79ca17a22bfa4754cdc8bf8ea25cb986d
Federico Gratti <federicogratti@gmail.com>
2021-12-06 14:11:04 -0300

    Improve update core script

---
+++ update_core.mjs
@@ -0,0 +1,7 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+
+let package_json = JSON.parse(fs.readFileSync('./package.json'));
+let core_version = package_json.dependencies['ohzi-core'];
+
+execSync('cd core && git fetch origin master && git checkout v' + core_version);
