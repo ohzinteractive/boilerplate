0bc56baf69392d1de6f25c5ed287c390b21db5cd
Federico Gratti <federicogratti@gmail.com>
2022-04-13 14:10:12 -0300

    Improve author log

--- app/js/PreLoader.js
+++ app/js/PreLoader.js
@@ -4,6 +4,7 @@ export default class PreLoader
   async init()
   {
     document.querySelector('.loader').classList.remove('hidden');
+    console.log(atob('RGVzaWduZWQgJiBEZXZlbG9wZWQgYnkgT0haSSBJbnRlcmFjdGl2ZSAvIGh0dHBzOi8vb2h6aS5pbw=='));
 
     const api = await import('./Api');
 
