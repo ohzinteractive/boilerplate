a43ca34a8d22c01206defe1b1ef9ad467d274660
Federico Gratti <federicogratti@gmail.com>
2022-11-07 08:51:16 +0100

    Update commands to support many submodules

--- update_core.mjs
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-import * as fs from 'fs';
-
-const package_json = JSON.parse(fs.readFileSync('./package.json'));
-const core_version = package_json.dependencies['ohzi-core'];
-
-execSync('cd core && git fetch origin main && git checkout v' + core_version);
