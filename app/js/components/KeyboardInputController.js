040fb19fc69c59933a3d6f9cdff885f3632c0ee3
Federico Gratti <federicogratti@gmail.com>
2023-05-18 16:02:59 -0300

    Update KeyboardInputController.js

--- app/js/components/KeyboardInputController.js
+++ app/js/components/KeyboardInputController.js
@@ -4,18 +4,22 @@ class KeyboardInputController
 {
   constructor()
   {
-    Input.keyboard.register_key('w');
-    Input.keyboard.register_key('s');
-    Input.keyboard.register_key('a');
-    Input.keyboard.register_key('d');
-    Input.keyboard.register_key('q');
-    Input.keyboard.register_key('e');
-    Input.keyboard.register_key('z');
-    Input.keyboard.register_key('c');
-    Input.keyboard.register_key('K');
+    Input.keyboard.register_key('KeyW');
+    Input.keyboard.register_key('ArrowUp');
+    Input.keyboard.register_key('KeyS');
+    Input.keyboard.register_key('ArrowDown');
+    Input.keyboard.register_key('KeyA');
+    Input.keyboard.register_key('ArrowLeft');
+    Input.keyboard.register_key('KeyD');
+    Input.keyboard.register_key('ArrowRight');
+    Input.keyboard.register_key('KeyQ');
+    Input.keyboard.register_key('KeyE');
+    Input.keyboard.register_key('KeyZ');
+    Input.keyboard.register_key('KeyC');
+    Input.keyboard.register_key('KeyK');
     Input.keyboard.register_key('Enter');
-    Input.keyboard.register_key('Shift');
-    Input.keyboard.register_key(' ');
+    Input.keyboard.register_key('ShiftRight');
+    Input.keyboard.register_key('Space');
   }
 }
 
