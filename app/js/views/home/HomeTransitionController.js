03b3c1c20a30ba6bb87ce3e7af7d3f388b588f66
Federico Gratti <federicogratti@gmail.com>
2025-09-23 16:37:11 -0300

    Fixed create-transition command

--- app/js/views/home/HomeTransitionController.js
+++ app/js/views/home/HomeTransitionController.js
@@ -1,14 +1,38 @@
 // import { TransitionManager } from 'ohzi-core';
+// import { ViewManager } from 'ohzi-core';
+// import { Sections } from '../Sections';
 import { SectionTransitionController } from '../common/SectionTransitionController';
 
 class HomeTransitionController extends SectionTransitionController
 {
+  start()
+  {
+    super.start();
+
+    // __CUSTOM_TRANSITIONS__
+  }
+
   before_enter()
   {
     super.before_enter();
 
     // Example of overriding initial state data
     // TransitionManager.current_state_data.home_opacity = 1;
+  }
+
+  on_enter()
+  {
+    super.on_enter();
+  }
+
+  update_enter_transition(global_view_data, transition_progress, action_sequencer)
+  {
+    super.update_enter_transition(global_view_data, transition_progress, action_sequencer);
+  }
+
+  update_exit_transition(global_view_data, transition_progress, action_sequencer)
+  {
+    super.update_exit_transition(global_view_data, transition_progress, action_sequencer);
   }
 }
 
